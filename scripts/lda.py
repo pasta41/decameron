1c871aa50617e071fa0bd10fb527d2eb5e9bf094
A.F. Cooper <afc78@cornell.edu>
2021-08-05 05:53:57 -0400

    update lda

--- scripts/lda.py
+++ scripts/lda.py
@@ -10,20 +10,43 @@ import pandas as pd
 import little_mallet_wrapper as lmw
 import pdb
 import constants
+import sys
 
 path_to_mallet = "~/mallet-2.0.8/bin/mallet"
 decameron_path = "/home/cooper/src/decameron/data/csv/decameron.csv"
 
 decameron_df = pd.read_csv(decameron_path)
+training_data = []
+story_ids = decameron_df["ID"].tolist()
+chunk_size = 100
 
+# TODO refactor
+def chunk_story(story_id, n):
+	story = decameron_df[decameron_df['ID']==story_id]
+	text = story['Text'].item()
+	# there has to be a better way to do this, but whatever
+	words = text.split()
+	# rejoin words every n words, put into list
+	story_chunks = [" ".join(words[i:i+n]) for i in range(0, len(words), n)]
+	return story_chunks
+	
 
-training_data = [lmw.process_string(t, 
-	stop_words=constants.stop_words) for t in decameron_df['Text'].tolist()]
-training_data = [d for d in training_data if d.strip()]
+if len(sys.argv) == 2 and sys.argv[1] == 'chunk':
+	training_data = []
+	for story_id in story_ids:
+		training_data.extend(chunk_story(story_id, chunk_size))
+
+	training_data = [lmw.process_string(t, 
+		stop_words=constants.stop_words) for t in training_data]
+	training_data = [d for d in training_data if d.strip()]
+else:
+	training_data = [lmw.process_string(t, 
+		stop_words=constants.stop_words) for t in decameron_df['Text'].tolist()]
+	training_data = [d for d in training_data if d.strip()]
 
 lmw.print_dataset_stats(training_data)
 
-num_topics = 10
+num_topics = 20
 output_directory_path = '/home/cooper/src/decameron/output'
 
 topic_keys, topic_distributions = lmw.quick_train_topic_model(path_to_mallet,
